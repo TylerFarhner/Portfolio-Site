71c30182bbacc0983caa2e5cc14b7c98fbb11468
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-25 00:50:30 -0800

    styling changes

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -3,15 +3,16 @@ import Carousel from 'react-bootstrap/Carousel'
 // IMAGES
 import HangLooseWebHome from '../images/HangLooseWebHome.png'
 import WorkItOut from '../images/WorkItOut.png'
+import HLM from '../images/HLM.mp4'
 
 
 export default function GalleryPage(props) {
     return (
         <main className="GalleryPage">
-            <h1>Gallery Page</h1>
+            <h1>Gallery</h1>
             <Carousel >
 
-                <Carousel.Item className="slide1">
+                <Carousel.Item >
                     <a href='https://workitout.netlify.app'>
                         <img
                         className="d-block w-100"
@@ -30,6 +31,15 @@ export default function GalleryPage(props) {
                         />
                     </a>
                 </Carousel.Item>
+
+                {/* <Carousel.Item>
+                    <div>
+                        <video style ={{width:"80%",height:"788px"}}>
+                            <source src={ HLM } type="video/mp4"></source>
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </Carousel.Item> */}
 
             </Carousel>
         </main>
