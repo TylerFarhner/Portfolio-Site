ccd91b4f200d6486731c100e6b4d28fa6f261b51
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-25 00:23:12 -0800

    opens links in new tab

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,9 +6,9 @@ export default function DashboardPage(props) {
             <main className="Page">
                 <h1 className="tylerFarhner">Tyler Farhner</h1>
                 <div className="externalLinks">
-                <a href="https://www.linkedin.com/in/tyfarhner/">LinkedIn</a>
-                <a href="https://github.com/TylerFarhner">GitHub</a>
-                <a href="https://drive.google.com/file/d/1VDQpnqBbBAn1g4AGxule_hhweE9UuOXg/view?usp=sharing">Resume</a>
+                <a className="BoxLink" href="https://www.linkedin.com/in/tyfarhner/" target="_blank">LinkedIn</a>
+                <a className="BoxLink" href="https://github.com/TylerFarhner" target="_blank">GitHub</a>
+                <a className="BoxLink" href="https://drive.google.com/file/d/1VDQpnqBbBAn1g4AGxule_hhweE9UuOXg/view?usp=sharing" target="_blank">Resume</a>
                 </div>
                 {/* <CarouselComponent /> */}
             </main>
