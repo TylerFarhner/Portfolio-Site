6d1f51e7daa82429dd0cf30928318fa913895b4a
Ty <69455296+TylerFarhner@users.noreply.github.com>
2021-02-22 09:18:47 -0800

    Fixes error, updates resume link

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -8,7 +8,7 @@ export default function DashboardPage(props) {
                 <div className="externalLinks">
                 <a className="BoxLink" href="https://www.linkedin.com/in/tyfarhner/" target="_blank" rel="noreferrer">LinkedIn</a>
                 <a className="BoxLink" href="https://github.com/TylerFarhner" target="_blank" rel="noreferrer">GitHub</a>
-                <a className="BoxLink" href="https://drive.google.com/file/d/10PmFyCItA68r1H6O6EUV6OogpFDuhVE9/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+                <a className="BoxLink" href="https://drive.google.com/file/d/1aXexts5zJGlemSB30xBQAciLaOBGgya4/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
                 </div>
                 {/* <CarouselComponent /> */}
             </main>
