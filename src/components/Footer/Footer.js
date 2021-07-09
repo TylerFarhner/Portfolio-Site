452c2c17453af8ec0677cef128e3eba383cf0b7e
Tyler Farhner <thecat@Tylers-MacBook-Pro.local>
2021-07-09 01:31:28 -0700

    changes copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer(props) {
     return (
         <footer>
-            <p>Copyright &copy; Tyler Farhner {new Date().getFullYear()} All Rights Reserved</p>
+            <p>Copyright &copy; King Industries {new Date().getFullYear()} All Rights Reserved</p>
         </footer>
     );
 }
